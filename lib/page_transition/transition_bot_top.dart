5e8e2122c99c09bebb6b4a711f863def3d8404d0
Thong <>
2019-09-29 00:14:02 +0800

    update animation after splash

--- lib/page_transition/transition_bot_top.dart
+++ lib/page_transition/transition_bot_top.dart
@@ -4,13 +4,13 @@ class TransitionBotTop extends PageRouteBuilder {
   final Widget child;
   TransitionBotTop({@required this.child})
       : super(
-            transitionDuration: Duration(milliseconds: 1000),
+            transitionDuration: Duration(milliseconds: 2000),
             pageBuilder: (context, animation, secondAnimation) => child,
             transitionsBuilder: (context, animation, secondAnimation, child) {
               final begin = Offset(0.0, 1.0);
               final end = Offset(0, 0);
               final tween = Tween(begin: begin, end: end)
-                  .chain(CurveTween(curve: Curves.easeIn));
+                  .chain(CurveTween(curve: Curves.easeInOutQuart));
               return SlideTransition(
                 position: animation.drive(tween),
                 child: child,
