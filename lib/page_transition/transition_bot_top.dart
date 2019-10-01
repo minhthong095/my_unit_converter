87f16df7102b45dc461195f004f10ae6fce4954c
Thong <>
2019-10-01 16:24:57 +0800

    change duration from splash to main

--- lib/page_transition/transition_bot_top.dart
+++ lib/page_transition/transition_bot_top.dart
@@ -4,7 +4,7 @@ class TransitionBotTop extends PageRouteBuilder {
   final Widget child;
   TransitionBotTop({@required this.child})
       : super(
-            transitionDuration: Duration(milliseconds: 2000),
+            transitionDuration: Duration(milliseconds: 800),
             pageBuilder: (context, animation, secondAnimation) => child,
             transitionsBuilder: (context, animation, secondAnimation, child) {
               final begin = Offset(0.0, 1.0);
